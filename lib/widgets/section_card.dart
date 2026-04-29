6a42aacba46542c97a4ab8e7f615ef756dda9549
Raiyanhq <raiyanhaque7@gmail.com>
2026-04-28 21:59:27 -0400

    Improve reusable card styling

--- lib/widgets/section_card.dart
+++ lib/widgets/section_card.dart
@@ -3,17 +3,31 @@ import 'package:flutter/material.dart';
 class SectionCard extends StatelessWidget {
   final Widget child;
   final EdgeInsetsGeometry padding;
+  final EdgeInsetsGeometry margin;
 
   const SectionCard({
     super.key,
     required this.child,
-    this.padding = const EdgeInsets.all(16),
+    this.padding = const EdgeInsets.all(18),
+    this.margin = const EdgeInsets.only(bottom: 16),
   });
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.only(bottom: 14),
+    return Container(
+      margin: margin,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(26),
+        border: Border.all(color: const Color(0xFFE2E8F0)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withValues(alpha: 0.04),
+            blurRadius: 20,
+            offset: const Offset(0, 10),
+          ),
+        ],
+      ),
       child: Padding(padding: padding, child: child),
     );
   }
